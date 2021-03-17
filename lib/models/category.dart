d1814477705bb86c339546f4e7afa9bf145db4a1
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-03-17 17:36:57 +0300

    json bağlantısı

---
+++ lib/models/category.dart
@@ -0,0 +1,20 @@
+
+import 'package:http_demo/main.dart';
+
+class Category {
+
+  int id;
+  String categoryName;
+  String seoUrl;
+
+  Category(this.id, this.categoryName, this.seoUrl);
+  Category.fromJson(Map json){
+    id= json["id"];
+    categoryName = json["categoryName"];
+    seoUrl = json["seoUrl"];
+
+  }
+  Map toJson(){
+    return {"id": id,"categoryName":categoryName, "seoUrl":seoUrl};
+  }
+}
